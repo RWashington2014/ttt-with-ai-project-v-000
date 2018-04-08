6b7f2fd859a9e7b9b145868c30d5a80302d993f8
Robert W <robwash@gmail.com>
2018-04-08 10:15:20 +0000

    Done.

---
+++ lib/board.rb
@@ -0,0 +1,19 @@
+class Board
+
+attr_accessor :cells
+
+
+  def initialize
+    @cells = Array.new(9, " ")
+  end
+
+  def
+
+  def reset!
+    @cells.clear
+    @cells = Array.new(9, " ")
+  end
+
+
+
+end
